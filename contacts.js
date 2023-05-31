37f9146487345e2927ad59262ed2017e0574b3ca
Andrii Valenko <andreyvalenko@gmail.com>
2023-05-31 16:33:31 +0300

    finish

--- contacts.js
+++ contacts.js
@@ -5,58 +5,42 @@ const { nanoid } = require("nanoid");
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 const listContacts = async () => {
-  try {
-    const response = await fs.readFile(contactsPath);
+  const response = await fs.readFile(contactsPath);
 
-    return JSON.parse(response) || null;
-  } catch (err) {
-    console.log(err);
-  }
+  return JSON.parse(response) || null;
 };
 
 const getContactById = async (contactId) => {
-  try {
-    const contacts = await listContacts();
+  const contacts = await listContacts();
 
-    const contact = contacts.find((contact) => contact.id === contactId);
+  const contact = contacts.find((contact) => contact.id === contactId);
 
-    return contact || null;
-  } catch (err) {
-    console.log(err);
-  }
+  return contact || null;
 };
 
 const removeContact = async (contactId) => {
-  try {
-    const contacts = await listContacts();
-    const index = contacts.findIndex((contact) => contact.id === contactId);
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
 
-    if (index === -1) return null;
+  if (index === -1) return null;
 
-    const [result] = contacts.splice(index, 1);
+  const [result] = contacts.splice(index, 1);
 
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-    return result;
-  } catch (err) {
-    console.log(err);
-  }
+  return result;
 };
 
 const addContact = async ({ name, email, phone }) => {
-  try {
-    const newContact = { id: nanoid(), name, email, phone };
+  const newContact = { id: nanoid(), name, email, phone };
 
-    const contacts = await listContacts();
+  const contacts = await listContacts();
 
-    contacts.push(newContact);
+  contacts.push(newContact);
 
-    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
-    return newContact;
-  } catch (err) {
-    console.log(err);
-  }
+  return newContact;
 };
 
 module.exports = { listContacts, getContactById, removeContact, addContact };
--- index.js
+++ index.js
@@ -19,25 +19,29 @@ program.parse();
 const options = program.opts();
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
-  switch (action) {
-    case "list":
-      const allContacts = await listContacts();
-      return console.log(allContacts);
+  try {
+    switch (action) {
+      case "list":
+        const allContacts = await listContacts();
+        return console.log(allContacts);
 
-    case "get":
-      const contact = await getContactById(id);
-      return console.log(contact);
+      case "get":
+        const contact = await getContactById(id);
+        return console.log(contact);
 
-    case "add":
-      const newContact = await addContact({ name, email, phone });
-      return console.log(newContact);
+      case "add":
+        const newContact = await addContact({ name, email, phone });
+        return console.log(newContact);
 
-    case "remove":
-      const deleteContact = await removeContact(id);
-      return console.log(deleteContact);
+      case "remove":
+        const deleteContact = await removeContact(id);
+        return console.log(deleteContact);
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  } catch (error) {
+    throw error;
   }
 };
 
